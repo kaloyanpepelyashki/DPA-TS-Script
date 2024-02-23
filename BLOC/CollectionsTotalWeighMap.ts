00653f54aad560ded764b0d0b02524892f264415
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2024-02-23 16:40:36 +0100

    Changed the calculation logic and split a method
    Changed the logic for calculating the products total weigh in the ProducsSoldMap class. Instead of calculating the products weight in kilogram within the initialize method, I moved the logic to the CollectionsTotalWeightMap class. This way, I avoid any errors that might be caused on the long way of several calculations. As javascript has problems with calculating floating points, I found it a more suitable solution to calculcate the floating point (kilograms) as last calculation, before returning the result.
    Also split the logic of the CollectionsTotalWeightMap class' method for getting the total weight of the collections into two smaller methods. I introduced a method that calculates the weight and then another mmethod converts it into kilograms and returns it. The logic could be split further into one more smaller method

--- BLOC/CollectionsTotalWeighMap.ts
+++ BLOC/CollectionsTotalWeighMap.ts
@@ -22,11 +22,15 @@ class CollectionsTotalWeightMap {
     }
   }
 
-  public async getCollectionsTotalWeight() {
+  /** This method calculates the collections total weight.
+   * It automatically intialises the objects needed to perform the calculation
+   */
+  protected async calculateCollectionsTotalWeight() {
     try {
-      let collectionsTotalWeightMap: Map<number, number> = new Map();
+      let collectionsWeight = {};
       await this.initialize();
 
+      //Iterates over the collectionsMap to map which product belongs to each collection
       this.collections.forEach((productIdArray, collectionId) => {
         let totalWeight = 0;
         this.soldProductsWeight.forEach((productValue, productId) => {
@@ -34,14 +38,49 @@ class CollectionsTotalWeightMap {
             totalWeight += productValue;
           }
         });
-        collectionsTotalWeightMap.set(collectionId, totalWeight);
+        collectionsWeight[collectionId] = totalWeight;
       });
 
-      return collectionsTotalWeightMap;
+      if (Object.keys(collectionsWeight).length !== 0) {
+        return collectionsWeight;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      console.log(`Error calculating collections total weigh: ${e.message}`);
+      return null;
+    }
+  }
+
+  /**This method returns a Map, encapsulating the collections weight in kilograms */
+  public async getCollectionsTotalWeight(): Promise<Map<
+    number,
+    number
+  > | null> {
+    try {
+      let collectionsTotalWeightMap: Map<number, number> = new Map();
+      const rawCollectionsWeight = await this.calculateCollectionsTotalWeight();
+
+      if (rawCollectionsWeight !== null) {
+        //Converting the collections weight from grams to kilograms
+        Object.keys(rawCollectionsWeight).forEach((collection) => {
+          const weightInKilograms = Number(
+            (rawCollectionsWeight[collection] / 1000).toFixed(3)
+          );
+          collectionsTotalWeightMap.set(Number(collection), weightInKilograms);
+        });
+
+        if (collectionsTotalWeightMap.size != null) {
+          return collectionsTotalWeightMap;
+        }
+      } else {
+        return null;
+      }
     } catch (e) {
       console.log(
         `Error getting the total weight for collections: ${e.message}`
       );
+      return null;
     }
   }
 }
