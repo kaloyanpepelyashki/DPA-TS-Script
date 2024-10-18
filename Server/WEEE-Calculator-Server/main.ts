2627b554d3ca33305cfe7e851f91180746491012
kaloyanpepelyashki <kaloyan.pepelyashki@gmail.com>
2024-10-18 08:23:13 +0200

    Introduced the webhooks router
    Introduced a separate rooter, that contains just the webhooks declarations.
    Additionally, intoduced some WebhookUtils

--- Server/WEEE-Calculator-Server/main.ts
+++ Server/WEEE-Calculator-Server/main.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import webHookRouter from "./Routes/webhooksRouter";
+
 //Services Imports
 import CollectionsTotalWeightMap from "./BLOC/CollectionsTotalWeighMap";
 import CollectionsManager from "./ServiceLayer/Services/CollectionsManager";
@@ -120,6 +122,9 @@ app.use((err, req: Request, res: Response, next: NextFunction) => {
   });
   res.status(500).send("Something went wrong! Internal server error");
 });
+
+//========= Web hook routes
+app.use("/webhooks", webHookRouter);
 
 //TODO Modify the neccessary methods to also require country the report is being exporeted for
 app.post("/api/v1/initCalculation", async (req: Request, res: Response) => {
