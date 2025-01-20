4bbcd3e1072fc93ec55d88024b833fbd162fc3a0
Kaloyan Pepelyashki <93147724+kaloyanpepelyashki@users.noreply.github.com>
2025-01-20 14:47:13 +0100

    Updated the deply yaml script file
    Uploaded the deploy script, to now create a env file on the server with the needed information.

--- Server/WEEE-Calculator-Server/Database/PGConnection.ts
+++ Server/WEEE-Calculator-Server/Database/PGConnection.ts
@@ -3,17 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PGSQL_USERNAME: string = "";
-const PGSQL_PASSWORD: string = "";
-const PGSQL_HOST: string = "";
-const PGSQL_PORT: number = 0;
-const PGSQL_DB_NAME: string = "";
+const PGSQL_USERNAME: string = process.env.PGSQL_USERNAME;
+const PGSQL_PASSWORD: string = process.env.PGSQL_PASSWORD;
+const PGSQL_HOST: string = process.env.PGSQL_HOST;
+const PGSQL_PORT: number = parseInt(process.env.PGSQL_PORT);
+const PGSQL_DB_NAME: string = process.env.PGSQL_DB_NAME;
 
 export default function initConnection() {
   try {
     const dbConnectionObject: ClientConfig = {
       user: PGSQL_USERNAME,
-      password: PGSQL_PASSWORD as string,
+      password: PGSQL_PASSWORD,
       host: PGSQL_HOST,
       port: PGSQL_PORT,
       database: PGSQL_DB_NAME,
